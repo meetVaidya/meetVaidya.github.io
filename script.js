434e13471ab995d3f6874b64065dd608219736f7
meext <vaidyameet2004@gmail.com>
2024-01-30 10:12:43 +0530

    Refactor button position and add media queries

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 const button = document.getElementById('myButton');
 
-function plzz() {
-    var positionRan = Math.floor(Math.random() * 1000);
+var positionRan;
+var inPos = String(positionRan) + 'px';
+
+function plzz(vp) {
+    positionRan = Math.floor(Math.random() * 1000);
+    if (positionRan > vh) {
+        plzz();
+    } else if (positionRan > vw) {
+        plzz();
+    }
     return String(positionRan) + 'px';
 }
 
-var positionRan = Math.floor(Math.random() * 1000);
-var inPos = String(positionRan) + 'px';
-
 button.addEventListener('click', () => {
     button.style.position = 'absolute';
-    button.style.top = plzz();
-    button.style.left = plzz();
+    button.style.top = plzz(vw);
+    button.style.left = plzz(vh);
+});
+
+let vw = Math.max(
+    document.documentElement.clientWidth || 0,
+    window.innerWidth || 0
+);
+let vh = Math.max(
+    document.documentElement.clientHeight || 0,
+    window.innerHeight || 0
+);
+
+button.addEventListener('click', () => {
+    console.log(vw, vh);
+    console.log(positionRan);
 });
---
+++ test.js
@@ -0,0 +1,10 @@
+let vw = Math.max(
+    document.documentElement.clientWidth || 0,
+    window.innerWidth || 0
+);
+let vh = Math.max(
+    document.documentElement.clientHeight || 0,
+    window.innerHeight || 0
+);
+
+console.log(vw, vh);
